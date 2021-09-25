487a5c5e7b5375744b435155b78aaa030cbcf40d
Dhikaadn <andhikadianpratama057@gmail.com>
2021-09-25 12:24:10 +0700

    Pertemuan 3

--- repo-minggu3/7.py
+++
@@ -1,6 +0,0 @@
-a = "UNIVERSITAS DIAN NUSWANTORO"
-b = "MAGERSARI"
-a1 = "UNIVERSITAS"
-b1 = "MAGERSARI"
-print("Kebalikan kata pertama UNIVERSITAS adalah ",a1[::-1])
-print("Kebalikan kata pertama MAGERSARI adalah ",b1[::-1])
